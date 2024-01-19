e4d9205b5617fa2b29ad3b9672792b681fe34356
Sameer <YOUR_EMAIL>
2024-01-19 19:01:16 +0530

    first commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+import bk from '../images/bk.jpg'
+
+
+
+const Home = () => {
+
+  return (
+
+    <div>
+      <div className="back">
+      <h1 className='text-4xl text-center p-10 font-bold text-gray-700'>Your Dream <span className='text-rose-500'>Home</span>, Your Cityscape.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
